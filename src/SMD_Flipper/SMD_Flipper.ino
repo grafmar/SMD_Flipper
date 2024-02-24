3e5653e61dca32267df1c85402989e6e8de6ee32
Marco Graf <33481763+grafmar@users.noreply.github.com>
2024-02-24 10:14:31 +0100

    Added Button input and cleanup

--- src/SMD_Flipper/SMD_Flipper.ino
+++ src/SMD_Flipper/SMD_Flipper.ino
@@ -1,17 +1,15 @@
-/* Sweep
- by BARRAGAN <http://barraganstudio.com>
- This example code is in the public domain.
-
- modified 8 Nov 2013
- by Scott Fitzgerald
- https://www.arduino.cc/en/Tutorial/LibraryExamples/Sweep
-*/
+/*
+ Control servos for SMD component flipper
+ */
 
 #include <Servo.h>
 
+const uint16_t servo1_pin = 9;
+const uint16_t servo2_pin = 10;
+const uint16_t button_pin = 8;
+
 Servo myservoA;  // create servo object to control a servo
 Servo myservoB;  // create servo object to control a servo
-// twelve servo objects can be created on most boards
 
 uint16_t posA = 0;    // variable to store the servo position
 uint16_t posB = 0;    // variable to store the servo position
@@ -19,27 +17,31 @@ uint16_t posB = 0;    // variable to store the servo position
 void setup() {
   myservoA.write(posA);              // tell servo to go to position in variable 'pos'
   myservoB.write(posB);              // tell servo to go to position in variable 'pos'
-  myservoA.attach(0);  // attaches the servo on pin 9 to the servo object
-  myservoB.attach(1);  // attaches the servo on pin 9 to the servo object
+  myservoA.attach(servo1_pin);  // attaches the servo on pin 9 to the servo object
+  myservoB.attach(servo2_pin);  // attaches the servo on pin 9 to the servo object
+
+  pinMode(servo1_pin, INPUT_PULLUP);
+  pinMode(servo2_pin, INPUT_PULLUP);
+  pinMode(button_pin, INPUT_PULLUP);
 }
 
 void loop() {
-  goPos(0,180);
-  delay(100);
-  goPos(180,0);
-  delay(100);
-  goPos(0,0);
-  delay(1000);
+  if (digitalRead(button_pin) == LOW) {
+    // power servos
+    pinMode(servo1_pin, OUTPUT);
+    pinMode(servo2_pin, OUTPUT);
+    // do movement
+    goPos(0,180,400);
+    goPos(180,0,400);
+    goPos(0,0,400);
+    // unpower servos
+    pinMode(servo1_pin, INPUT_PULLUP);
+    pinMode(servo2_pin, INPUT_PULLUP);
+  }
 }
 
-void goPos(uint16_t a,uint16_t b) {
-  while((posA!=a)||(posB!=b)) {
-    if(posA>a) posA--;
-    if(posA<a) posA++;
-    if(posB>b) posB--;
-    if(posB<b) posB++;
-    myservoA.write(posA);
-    myservoB.write(posB);
-    delay(3);
-  }
+void goPos(uint16_t a,uint16_t b,uint16_t delayForMovement) {
+  myservoA.write(a);
+  myservoB.write(b);
+  delay(delayForMovement);
 }
